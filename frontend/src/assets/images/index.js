f827f37c49eb7a8b4f5f7ce0786ab944a88944c1
poojaramnaney <poojaramnaney@gmail.com>
2023-05-24 01:22:48 +0530

    frontend updated

--- frontend/src/assets/images/index.js
+++
@@ -1,7 +0,0 @@
-import blog1 from "../images/blog/blog1.png";
-import blog2 from "../images/blog/blog2.png";
-import blog3 from "../images/blog/blog3.png";
-import blog4 from "../images/blog/blog4.png";
-import blog5 from "../images/blog/blog5.png";
-
-export {blog1, blog2, blog3, blog4, blog5};
---
+++ frontend/src/components/Testinomialimg.jsx
@@ -0,0 +1,233 @@
+import React from "react";
+import { test1 } from "../assets";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInstagram, faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faFacebook } from "@fortawesome/free-brands-svg-icons";
+const Testinomialimg = () => {
+  return (
+    <div className="testimonial-bg  md:p-8">
+       <div className="flex justify-center text-center">
+              <h1 className="text-black font-bold text-center">Our </h1>
+              <h1 className="text-[#5D9943] font-bold text-center">Testimonials</h1>
+            </div>
+            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-2 gap-x-32  mx-24 my-14">
+              <div className="flex w-[1200px]">
+            <div className="bg-white rounded-lg shadow-lg overflow-hidden mr-10 mt-10 br-0 h-72  w-6/12">
+              <div className="flex items-center">
+                <div className="w-1/3">
+                  <img
+                    className="ml-12 mt-3 rounded-l-full w-28 h-28"
+                    src={test1}
+                    alt="alt"
+                  />
+                </div>
+                <div className="mt-3 w-1/3 py-4">
+                  <p className="text-gray-800 font-bold text-xl mb-2">
+                    Jane Doe
+                  </p>
+                  <p className="text-gray-500 text-sm mb-2">
+                    Full stack developer
+                  </p>
+                  <p className="text-gray-400 text-base  w-64">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam.
+                  </p>
+                </div>
+              </div>
+              <div className="flex gap-3">
+                <a href="#" className="text-white ">
+                  <FontAwesomeIcon
+                    icon={faInstagram}
+                    className=" pl-40 text-2xl  ml-20  text-blue-500 hover:text-blue-700"
+                  />
+                </a>
+                <a href="#" className="text-white ">
+                  <FontAwesomeIcon
+                    icon={faFacebook}
+                    className="text-2xl text-blue-500 hover:text-blue-700"
+                  />
+                </a>
+                <a href="#" className="text-white ">
+                  <FontAwesomeIcon
+                    icon={faLinkedinIn}
+                    className="text-2xl text-blue-500 hover:text-blue-700"
+                  />
+                </a>
+                <a href="#" className="text-white ">
+                  <FontAwesomeIcon
+                    icon={faTwitter}
+                    className="text-2xl  text-blue-500 hover:text-blue-700"
+                  />
+                </a>
+              </div>
+            </div>
+            </div>
+            <div className="flex w-[1200px]">
+            <div className="bg-white rounded-lg shadow-lg overflow-hidden mr-10 mt-10 br-0 h-72  w-6/12">
+              <div className="flex items-center">
+                <div className="w-1/3">
+                  <img
+                    className="ml-12 mt-3 rounded-l-full w-28 h-28"
+                    src={test1}
+                    alt="alt"
+                  />
+                </div>
+                <div className="mt-3 w-1/3 py-4">
+                  <p className="text-gray-800 font-bold text-xl mb-2">
+                    Jane Doe
+                  </p>
+                  <p className="text-gray-500 text-sm mb-2">
+                    Full stack developer
+                  </p>
+                  <p className="text-gray-400 text-base  w-64">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam.
+                  </p>
+                </div>
+              </div>
+              <div className="flex gap-3">
+                <a href="#" className="text-white ">
+                  <FontAwesomeIcon
+                    icon={faInstagram}
+                    className=" pl-40 text-2xl  ml-20  text-blue-500 hover:text-blue-700"
+                  />
+                </a>
+                <a href="#" className="text-white ">
+                  <FontAwesomeIcon
+                    icon={faFacebook}
+                    className="text-2xl text-blue-500 hover:text-blue-700"
+                  />
+                </a>
+                <a href="#" className="text-white ">
+                  <FontAwesomeIcon
+                    icon={faLinkedinIn}
+                    className="text-2xl text-blue-500 hover:text-blue-700"
+                  />
+                </a>
+                <a href="#" className="text-white ">
+                  <FontAwesomeIcon
+                    icon={faTwitter}
+                    className="text-2xl  text-blue-500 hover:text-blue-700"
+                  />
+                </a>
+              </div>
+            </div>
+            </div>
+
+            </div>
+            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-2 gap-x-32  mx-24 my-14">
+              <div className="flex w-[1200px]">
+            <div className="bg-white rounded-lg shadow-lg overflow-hidden mr-10 mt-10 br-0 h-72  w-6/12">
+              <div className="flex items-center">
+                <div className="w-1/3">
+                  <img
+                    className="ml-12 mt-3 rounded-l-full w-28 h-28"
+                    src={test1}
+                    alt="alt"
+                  />
+                </div>
+                <div className="mt-3 w-1/3 py-4">
+                  <p className="text-gray-800 font-bold text-xl mb-2">
+                    Jane Doe
+                  </p>
+                  <p className="text-gray-500 text-sm mb-2">
+                    Full stack developer
+                  </p>
+                  <p className="text-gray-400 text-base  w-64">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam.
+                  </p>
+                </div>
+              </div>
+              <div className="flex gap-3">
+                <a href="#" className="text-white ">
+                  <FontAwesomeIcon
+                    icon={faInstagram}
+                    className=" pl-40 text-2xl  ml-20  text-blue-500 hover:text-blue-700"
+                  />
+                </a>
+                <a href="#" className="text-white ">
+                  <FontAwesomeIcon
+                    icon={faFacebook}
+                    className="text-2xl text-blue-500 hover:text-blue-700"
+                  />
+                </a>
+                <a href="#" className="text-white ">
+                  <FontAwesomeIcon
+                    icon={faLinkedinIn}
+                    className="text-2xl text-blue-500 hover:text-blue-700"
+                  />
+                </a>
+                <a href="#" className="text-white ">
+                  <FontAwesomeIcon
+                    icon={faTwitter}
+                    className="text-2xl  text-blue-500 hover:text-blue-700"
+                  />
+                </a>
+              </div>
+            </div>
+            </div>
+            <div className="flex w-[1200px]">
+            <div className="bg-white rounded-lg shadow-lg overflow-hidden mr-10 mt-10 br-0 h-72  w-6/12">
+              <div className="flex items-center">
+                <div className="w-1/3">
+                  <img
+                    className="ml-12 mt-3 rounded-l-full w-28 h-28"
+                    src={test1}
+                    alt="alt"
+                  />
+                </div>
+                <div className="mt-3 w-1/3 py-4">
+                  <p className="text-gray-800 font-bold text-xl mb-2">
+                    Jane Doe
+                  </p>
+                  <p className="text-gray-500 text-sm mb-2">
+                    Full stack developer
+                  </p>
+                  <p className="text-gray-400 text-base  w-64">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam.
+                  </p>
+                </div>
+              </div>
+              <div className="flex gap-3">
+                <a href="#" className="text-white ">
+                  <FontAwesomeIcon
+                    icon={faInstagram}
+                    className=" pl-40 text-2xl  ml-20  text-blue-500 hover:text-blue-700"
+                  />
+                </a>
+                <a href="#" className="text-white ">
+                  <FontAwesomeIcon
+                    icon={faFacebook}
+                    className="text-2xl text-blue-500 hover:text-blue-700"
+                  />
+                </a>
+                <a href="#" className="text-white ">
+                  <FontAwesomeIcon
+                    icon={faLinkedinIn}
+                    className="text-2xl text-blue-500 hover:text-blue-700"
+                  />
+                </a>
+                <a href="#" className="text-white ">
+                  <FontAwesomeIcon
+                    icon={faTwitter}
+                    className="text-2xl  text-blue-500 hover:text-blue-700"
+                  />
+                </a>
+              </div>
+            </div>
+            </div>
+
+            </div>
+    </div>
+  );
+};
+
+export default Testinomialimg;
